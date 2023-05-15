41eb6092b6efd79de9e6c1b99cd94dbe2ec066b6
Heechul Kim <jijisa@iorchard.net>
2023-05-15 18:21:07 +0900

    updated burrito script

--- rocky/burrito_build.sh
+++ rocky/burrito_build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-VER=${1:-8.7}
-ISO_PATH="/data/jijisa/images/burrito-${VER}.iso"
-DISK_PATH="/data/jijisa/images/Burrito-GenericCloud-${VER}-$(date +%Y%m%d%H%M)-x86_64.qcow2"
+VER=${1:-1.1.0}
+OS_VER=${2:-8.7}
+ISO_PATH="/data/jijisa/images/burrito-${VER}_${OS_VER}.iso"
+DISK_PATH="/data/jijisa/images/Burrito-GenericCloud-${VER}_${OS_VER}-$(date +%Y%m%d%H%M)-x86_64.qcow2"
 DISK_SIZE=5
 DISK_FORMAT="qcow2"
 NET_BRIDGE="br1"
+VMNAME="burrito-8"
+
+if $(virsh list --all --name | grep -q "^${VMNAME}$"); then
+  if $(virsh list --name | grep -q "^${VMNAME}$"); then
+    virsh destroy $VMNAME
+  fi
+  virsh undefine $VMNAME
+fi
 
 virt-install \
   --name burrito-8 \
--- rocky/validate_iso_vnc.sh
+++ rocky/validate_iso_vnc.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 set -euo pipefail
 
-BOOT_OPT=${1:-legacy}
+VMNAME="pbos-validate"
+
+#BOOT_OPT=${1:-legacy}
+BOOT_OPT=${1:-}
 if [ "x$BOOT_OPT" != "xlegacy" ] && [ "x$BOOT_OPT" != "xuefi" ]; then
-  echo "Abort: Unknown $BOOT_OPT. It should be 'legacy' or 'uefi'."
+  echo "Usage) $0 legacy|uefi"
+  echo "Abort: Unknown boot option $BOOT_OPT. It should be 'legacy' or 'uefi'."
   exit 1
 fi
-[ "x$BOOT_OPT" = "xuefi" ] && BOOT="--boot uefi" || true
-ISO_PATH="/data/jijisa/images/pbos-8.5-2202.iso"
-DISK_PATH="/data/jijisa/images/pbos-${BOOT_OPT}.qcow2"
+[ "x$BOOT_OPT" = "xuefi" ] && BOOT="--boot uefi" || BOOT=""
+ISO_PATH="/data/jijisa/images/pbos-8.5-2202-2.iso"
+DISK_PATH="/data/jijisa/images/${VMNAME}-${BOOT_OPT}.qcow2"
 DISK_SIZE=5
 DISK_FORMAT="qcow2"
 NET_BRIDGE="br1"
 
+# check instance already exists and if it does, destroy and undefine it.
+if $(virsh list --all --name | grep -q "^${VMNAME}$"); then
+  if $(virsh list --name | grep -q "^${VMNAME}$"); then
+    virsh destroy $VMNAME
+  fi
+  virsh undefine $VMNAME
+fi
+
 virt-install \
-  --name pbos-validate $BOOT \
+  --name $VMNAME $BOOT \
   --memory=1024 \
   --vcpus=1 \
   --os-type linux \
